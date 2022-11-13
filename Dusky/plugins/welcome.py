5f00f22dad864eed5120ddb9690999342e868c06
Dusky Joe (Eshwaran) <116634369+DuskyMusic@users.noreply.github.com>
2022-11-13 04:20:04 -0800

    Update welcome.py

--- Dusky/plugins/welcome.py
+++ Dusky/plugins/welcome.py
@@ -188,13 +188,13 @@ async def welcome(_, message: Message):
                 await message.reply_text(
                     f"""
 Thanks for adding me to your {group_name}! Don't forget follow
-my news channel @Theszrosebot.
+my news channel @DuskysUpdates.
 
 **New to Me, Touch the below button and start me in PM**
                     """,
                     reply_markup=InlineKeyboardMarkup(
             [
-                InlineKeyboardButton("quick start guide", url="http://t.me/szrosebot?start=help"),
+                InlineKeyboardButton("quick start guide", url="http://t.me/DuskyRobot?start=help"),
             ]))
                 await app.send_message(
                 chat_id=LOG_GROUP_ID,
@@ -210,7 +210,7 @@ my news channel @Theszrosebot.
                 "Wow ! Owner has just joined your chat.",
             )
                return
-        if user_id == 1467358214:#for @supunma 
+        if user_id == 5696423555:#for @OFFICIAL_DUSKY 
                await app.send_message(
                 message.chat.id,
                 "Wow ! Developer has just joined your chat.",
